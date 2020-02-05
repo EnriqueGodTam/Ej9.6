cc455c2817c1fd473d46a15b277189c16244edc4
enriq <enriq@DESKTOP-OBF17AV>
2020-02-05 11:42:15 +0100

    prueba

---
+++ src/ej9/pkg6/Alumno.java
@@ -0,0 +1,17 @@
+package ej9.pkg6;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author enriq
+ */
+public class Alumno {
+   private static int num_asignaturas = 5; 
+   private String nombre;
+   
+}
---
+++ src/ej9/pkg6/Ej96.java
@@ -0,0 +1,10 @@
+
+package ej9.pkg6;
+
+public class Ej96 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
