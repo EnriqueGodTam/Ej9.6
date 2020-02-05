d7663a17513ad20ba5ac6eca5cd0fb935c358f5b
enriq <enriq@DESKTOP-OBF17AV>
2020-02-05 11:50:01 +0100

    prueba

--- src/ej9/pkg6/Alumno.java
+++ src/ej9/pkg6/Alumno.java
@@ -1,5 +1,7 @@
 package ej9.pkg6;
 
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,5 +15,19 @@ package ej9.pkg6;
 public class Alumno {
    private static int num_asignaturas = 5; 
    private String nombre;
+   private ArrayList <Double> notas;
+   
+   public Alumno (){
+       num_asignaturas = 5;
+       nombre = "";
+       notas = new ArrayList <> ();
+   }
+   
+   public Alumno (String nombre){
+       num_asignaturas = 5;
+       this.nombre = nombre;
+       notas = new ArrayList <> ();
+   }
+   
    
 }
