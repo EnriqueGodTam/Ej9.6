d0e9672210146e25aa217468732cd5cde30cc64e
enriq <enriq@DESKTOP-OBF17AV>
2020-02-05 12:00:06 +0100

    prueba

--- src/ej9/pkg6/Alumno.java
+++ src/ej9/pkg6/Alumno.java
@@ -1,6 +1,7 @@
 package ej9.pkg6;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -38,6 +39,18 @@ public class Alumno {
     }
 
     public void pedirNotas () throws RangoException {
-        
+        double nota;
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Introduzca las notas del alumno: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Nota " + (i+1));
+            nota = teclado.nextDouble();
+            
+            if (nota < 0 || nota > 10){
+                throw new RangoException ("El rango de la nota no esta permitido");
+            }else{
+                notas.add(nota);
+            }
+        }
     }
 }
